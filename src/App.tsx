9657b501092d4a9ef425bc3db68c37ad91f9bc84
ayush2723 <ayush1289sharma@gmail.com>
2025-06-24 01:50:03 +0530

    changes for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection.tsx';
